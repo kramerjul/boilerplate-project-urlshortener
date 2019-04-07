58af7b8621eee5c8e3002b9a562de608560e854f
Julian <kramerjul@gmail.com>
2019-04-07 12:11:32 +0200

    added POST endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
+var bodyParser = require('body-parser');
 
 var cors = require('cors');
 
@@ -15,23 +16,32 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-//mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var exampleUrl = {
+  original_url: "http://google.com",
+  shortend_url: 1
+}
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/shorturl/:urlId?", function (req, res) {
+  //res.json(exampleUrl);
+  res.redirect(exampleUrl.original_url);
+});
+
+app.post("/api/shorturl/new/", function(req, res){
+  res.json({original_url: req.body.url, short_url: "id_tbd"});
 });
 
 
